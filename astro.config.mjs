e0fa91dd0b2b6a94714f0753178ada12e6c0c24d
Mikko Sanera <rns.jey@gmail.com>
2025-10-08 16:16:51 +0100

    Add Preact to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@tailwindcss/vite";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroblog.sanerasolutions.com/",
+
   vite: {
     plugins: [tailwind()],
   },
+
+  integrations: [preact()],
 });
